c3c7c09b9af7c7383823eada2101031962eb7c26
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-03-20 12:41:06 +0000

    wrapped Home within StatelessWidget for simultaneous change on emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('First App'),
-      centerTitle: true,
-      backgroundColor: Colors.green[600],
-    ),
-    body: Center(
-      child: Text(
-        'The body of the app in the middle',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.redAccent[700],
-          fontFamily: 'BalooThambi',
-        )
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('button'),
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override //redifines the build method otherwise inherited from StatelessWidget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+            'The body of the app in the middle',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.redAccent[700],
+              fontFamily: 'BalooThambi',
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('button'),
+      ),
+    );
+  }
+}
+
