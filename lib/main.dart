853b2e70dfc8316683d18805e64a3586d6ab59ef
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-03-20 11:52:30 +0000

    style text with imported font in body

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Ciao Marcello'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('First App'),
+      centerTitle: true,
+      backgroundColor: Colors.green[600],
+    ),
+    body: Center(
+      child: Text(
+        'The body of the app in the middle',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.redAccent[700],
+          fontFamily: 'BalooThambi',
+        )
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('button'),
+    ),
+  ),
 ));
